ef0518ebc85dae04938e8c3ef7a9462dc93130a5
Recep <recep.yildiz@hva.nl>
2025-01-16 21:39:34 +0100

    #18 Basis opstelling modal pop up

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,14 @@
-const readButton = document.querySelector('.read-button');
+const modal = document.querySelector('#modal');
+const openModal = document.querySelector('.open-button');
+const closeModal = document.querySelector('.close-button');
 
 
+openModal.addEventListener('click', () => {
+    modal.showModal();
+})
+
+closeModal.addEventListener('click', () => {
+    modal.close();
+})
+
+// console.log(modal, openModal, closeModal);
